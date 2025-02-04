cef0bffe47ad53dee7736a1a4f1d3e12116897e2
Cody L Hall <chal032687@gmail.com>
2025-02-04 01:11:33 -0500

    netlify

--- src/components/FooterSimple.tsx
+++ src/components/FooterSimple.tsx
@@ -1,4 +1,4 @@
-import { Container, Text} from '@mantine/core';
+import { Text} from '@mantine/core';
 import classes from './FooterSimple.module.css';
 
 
@@ -7,11 +7,9 @@ export function FooterSimple() {
 
   return (
     <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <Text size="sm" >
+      <Text size="sm" >
       © {new Date().getFullYear()} Cody L. Hall. All rights reserved.
       </Text>
-      </Container>
     </div>
   );
 }
